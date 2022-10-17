1e0483dbe40c2d58ce53629d526f2fbc307a58e2
Joshua Booth <me@joshuabooth.nz>
2022-10-17 23:03:58 +1300

    fix: use correct build folder

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import svgrPlugin from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), svgrPlugin()],
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+  },
   test: {
     globals: true,
     environment: "jsdom",
